0fe4ee83b117ab8365224902164484ae2f9f114a
BhanuZopsmart <bhanu.bisen@zopsmart.com>
2022-02-14 16:08:27 +0530

    bhanu/BhanuZopsmart

--- Match 03/submissions/base.js
+++ Match 03/submissions/base.js
@@ -1,6 +1,8 @@
 function solution(a, b) {
+  if(a.length !=b.length){return false;}
   let flag=0;
   var x=a.toUpperCase().split("").sort(),y=b.toUpperCase().split("").sort();
+  
   if (x.length==y.length){
     for(let i=0;i<x.length;i++){
       if(x[i]==y[i] && x[i]>='A' && x[i]<='Z'&& y[i]>='A'&& y[i]<='Z'){

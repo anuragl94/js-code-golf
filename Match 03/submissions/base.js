906d4750ae740c20e23b4f032d86851db80e9acd
Nikhil <nikhil.chandna@zopsmart.com>
2022-02-14 12:45:01 +0530

    match3 complete

--- Match 03/submissions/base.js
+++ Match 03/submissions/base.js
@@ -1,40 +1,14 @@
 function solution(a, b) {
+
+  let arr1=[...a];
+  let arr2=[...b];
+  arr1.sort();
+  arr2.sort();
   
-  if(a.length!=b.length)
-    return false
-
-  let map1 = {};
-
-  console.log(map1);
-
-  for(let i=0;i<a.length;i++)
+  for(let i=0;i<arr1.length;i++)
   {
-    if(a[i] in map1)
-    {
-      map1[a[i]]=map1[a[i]]+1;
-    }
-    else
-      map1[a[i]]=1;
+    if(arr1[i]!=arr2[i])
+      return false
   }
-
-  console.log(map1);
-  
-  for(let i=0;i<b.length;i++)
-  {
-    if(b[i] in map1)
-    {
-      map1[b[i]]=map1[b[i]]-1;
-    }
-  }
-  
-  
-  for(const key in map1)
-  {
-    if(map1[key]!=0)
-    return false;
-  }
-
-  
   return true;
-
 }

434d00b9e52be2defef17f971573f9c1f8d423fb
ayush070zopsmart <ayush.agrawal@zopsmart.com>
2022-02-14 19:34:30 +0530

    Match 02 partially done match 03 completed

---
+++ Match 02/submissions/ayush.agrawal@zopsmart.com-ayush070zopsmart.js
@@ -0,0 +1,69 @@
+function solution() {
+
+    return `when i find myself in times of trouble
+mother mary comes to me
+speaking words of wisdom, let it be
+and in my hour of darkness
+she is standing right in front of me
+speaking words of wisdom, let it be
+let it be, let it be
+let it be, let it be
+whisper words of wisdom, let it be
+and when the broken-hearted people
+living in the world agree
+there will be an answer, let it be
+for though they may be parted
+there is still a chance that they will see
+there will be an answer, let it be
+let it be, let it be
+let it be, let it be
+yeah, there will be an answer, let it be
+let it be, let it be
+let it be, let it be
+whisper words of wisdom, let it be
+let it be, let it be
+ah, let it be, yeah, let it be
+whisper words of wisdom, let it be
+and when the night is cloudy
+there is still a light that shines on me
+shine on until tomorrow, let it be
+i wake up to the sound of music,
+mother mary comes to me
+speaking words of wisdom, let it be
+let it be, let it be
+let it be, yeah, let it be
+oh, there will be an answer, let it be
+let it be, let it be
+let it be, yeah, let it be
+whisper words of wisdom, let it be`;
+}
+
+function removeDuplicates() {
+    let lines = solution().split("\n");
+    let lyric = [];
+    // console.log(p);
+    lines.map(e => {
+        lyric.push(...e.split(", "));
+    });
+    let freq = {};
+    let arr = [];
+    lyric.forEach(function(value, index) {
+        if (!freq[value]) {
+            arr.push(value);
+        }
+        freq[value] = freq[value] || [];
+        freq[value].push(index);
+
+    });
+    console.log(freq)
+    console.log(arr);
+    // return arr[0] + '\n' +
+    //     arr[1] + '\n' +
+    //     arr[2] + ', ' + arr[3] + '\n' +
+    //     arr[4] + '\n' +
+    //     arr[5] + '\n' +
+    //     arr[6] + ', ' + '\n' + arr[7] + '\n' + arr[8] + '\n' +
+    //     arr[9] + '\n' + arr[10] + '\n' + arr[2] + ', ' + arr[3] + '\n' + arr[4] + '\n' + arr[5] + '\n' + arr[6] + '\n' +
+    //     arr[7] + '\n' + arr[8] + '\n' + arr[9] + '\n' + arr[10] + '\n';
+
+}

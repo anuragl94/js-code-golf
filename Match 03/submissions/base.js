5f466e0228941f5c3d78298a7242b0477a47c9db
atishkushwaha <atish.kushwaha@zopsmart.com>
2022-02-14 16:16:39 +0530

    modified

--- Match 03/submissions/base.js
+++ Match 03/submissions/base.js
@@ -2,7 +2,7 @@ function solution(a, b) {
   var x=Array.from(a.toUpperCase()).sort(),y=Array.from(b.toUpperCase()).sort(),flag=0;
   if(x.length==y.length){
     for(var i=0;i<x.length;i++)
-         if(x[i]==y[i] && x[i]>='A' && x[i]<='Z' && y[i]>='A' && y[i]<='Z')
+         if(x[i]==y[i] && x[i]>='A'&&x[i]<='Z')
          continue;
         else flag=1;
   }else flag=1;

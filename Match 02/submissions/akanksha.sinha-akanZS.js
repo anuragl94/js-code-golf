02578fc9f2bb0aee075552478f93a11caf0aaabd
akanZS <akanksha.sinha@zopsmart.com>
2022-02-14 16:55:45 +0530

    fix-2 3

---
+++ Match 02/submissions/akanksha.sinha-akanZS.js
@@ -0,0 +1,41 @@
+function solution() {
+    let str1 = "let it be"
+    let str2 = "whisper words of wisdom,"
+    return `when i find myself in times of trouble
+  mother mary comes to me
+  speaking words of wisdom, ${str1}
+  and in my hour of darkness
+  she is standing right in front of me
+  speaking words of wisdom, ${str1}
+  ${str1}, ${str1}
+  ${str1}, ${str1}
+  ${str2} ${str1}
+  and when the broken-hearted people
+  living in the world agree
+  there will be an answer, ${str1}
+  for though they may be parted
+  there is still a chance that they will see
+  there will be an answer, ${str1}
+  ${str1}, ${str1}
+  ${str1}, ${str1}
+  yeah, there will be an answer, ${str1}
+  ${str1}, ${str1}
+  ${str1}, ${str1}
+  ${str2} ${str1}
+  ${str1}, ${str1}
+  ah, ${str1}, yeah, ${str1}
+  ${str2} ${str1}
+  and when the night is cloudy
+  there is still a light that shines on me
+  shine on until tomorrow, ${str1}
+  i wake up to the sound of music,
+  mother mary comes to me
+  speaking words of wisdom, ${str1}
+  ${str1}, ${str1}
+  ${str1}, yeah, ${str1}
+  oh, there will be an answer, ${str1}
+  ${str1}, ${str1}
+  ${str1}, yeah, ${str1}
+  ${str2} ${str1}`;
+  
+  }

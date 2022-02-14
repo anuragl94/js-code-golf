8037e46e57052ddf39553ba6af27fd952290e484
atishkushwaha <atish.kushwaha@zopsmart.com>
2022-02-14 15:04:26 +0530

    changed

--- Match 02/submissions/base.js
+++ Match 02/submissions/base.js
@@ -1,38 +1,40 @@
 function solution() {
+  let ltb = "let it be";
+  let tba = "there will be an answer";
   return `when i find myself in times of trouble
 mother mary comes to me
-speaking words of wisdom, let it be
+speaking words of wisdom, ${ltb}
 and in my hour of darkness
 she is standing right in front of me
-speaking words of wisdom, let it be
-let it be, let it be
-let it be, let it be
-whisper words of wisdom, let it be
+speaking words of wisdom, ${ltb}
+${ltb}, ${ltb}
+${ltb}, ${ltb}
+whisper words of wisdom, ${ltb}
 and when the broken-hearted people
 living in the world agree
-there will be an answer, let it be
+${tba}, ${ltb}
 for though they may be parted
 there is still a chance that they will see
-there will be an answer, let it be
-let it be, let it be
-let it be, let it be
-yeah, there will be an answer, let it be
-let it be, let it be
-let it be, let it be
-whisper words of wisdom, let it be
-let it be, let it be
-ah, let it be, yeah, let it be
-whisper words of wisdom, let it be
+${tba}, ${ltb}
+${ltb}, ${ltb}
+${ltb}, ${ltb}
+yeah, ${tba}, ${ltb}
+${ltb}, ${ltb}
+${ltb}, ${ltb}
+whisper words of wisdom, ${ltb}
+${ltb}, ${ltb}
+ah, ${ltb}, yeah, ${ltb}
+whisper words of wisdom, ${ltb}
 and when the night is cloudy
 there is still a light that shines on me
-shine on until tomorrow, let it be
+shine on until tomorrow, ${ltb}
 i wake up to the sound of music,
 mother mary comes to me
-speaking words of wisdom, let it be
-let it be, let it be
-let it be, yeah, let it be
-oh, there will be an answer, let it be
-let it be, let it be
-let it be, yeah, let it be
-whisper words of wisdom, let it be`;
+speaking words of wisdom, ${ltb}
+${ltb}, ${ltb}
+${ltb}, yeah, ${ltb}
+oh, ${tba}, ${ltb}
+${ltb}, ${ltb}
+${ltb}, yeah, ${ltb}
+whisper words of wisdom, ${ltb}`;
 }
--- Match 03/submissions/base.js
+++ Match 03/submissions/base.js
@@ -1,3 +1,10 @@
 function solution(a, b) {
-  return false;
+  var x=Array.from(a.toUpperCase()).sort(),y=Array.from(b.toUpperCase()).sort(),flag=0;
+  if(x.length==y.length){
+    for(var i=0;i<x.length;i++)
+         if(x[i]==y[i] && x[i]>='A' && x[i]<='Z' && y[i]>='A' && y[i]<='Z')
+         continue;
+        else flag =1;
+  }else flag=1;
+  if(flag==1) return false;else return true;
 }

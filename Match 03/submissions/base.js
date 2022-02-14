6788c8ccfdcc4339bfa085a27ff78111cbd3c3ec
Nikhil <nikhil.chandna@zopsmart.com>
2022-02-14 12:22:43 +0530

    match 3 completed

--- Match 03/submissions/base.js
+++ Match 03/submissions/base.js
@@ -1,3 +1,40 @@
 function solution(a, b) {
-  return false;
+  
+  if(a.length!=b.length)
+    return false
+
+  let map1 = {};
+
+  console.log(map1);
+
+  for(let i=0;i<a.length;i++)
+  {
+    if(a[i] in map1)
+    {
+      map1[a[i]]=map1[a[i]]+1;
+    }
+    else
+      map1[a[i]]=1;
+  }
+
+  console.log(map1);
+  
+  for(let i=0;i<b.length;i++)
+  {
+    if(b[i] in map1)
+    {
+      map1[b[i]]=map1[b[i]]-1;
+    }
+  }
+  
+  
+  for(const key in map1)
+  {
+    if(map1[key]!=0)
+    return false;
+  }
+
+  
+  return true;
+
 }

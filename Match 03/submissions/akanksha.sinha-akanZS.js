36a6e546faf04fe3b275cf9ab2e41e7c58c2162c
akanZS <akanksha.sinha@zopsmart.com>
2022-02-15 17:32:34 +0530

    coorected

--- Match 03/submissions/akanksha.sinha-akanZS.js
+++
@@ -1,9 +0,0 @@
-function solution(str1, str2){
-    let sChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
-    if(sChar.test(str1) || sChar.test(str2)) return false;
-    
-    let a = str1.split('').sort().join('')
-    let b = str2.split('').sort().join('')
-
-    return (a === b)
-}

95241d364ab13b3557affcfbb45cbb8271fa14e3
gitapoorv <apoorv.gupta@zopsmart.com>
2022-02-25 13:17:53 +0530

    updated question 2 & 3

---
+++ Match 02/submissions/apoorv.gupta.js
@@ -0,0 +1,18 @@
+function compressStr() {
+  const a = "let it be";
+  const aa = `\n${a}, ${a}\n${a}, ${a}\n`;
+  const ay = `\n${a}, ${a}\n${a}, yeah, ${a}\n`;
+  const b = "\nmother mary comes to me";
+  const c = "words of wisdom";
+  const d = "\nspeaking";
+  const e = "\nwhisper";
+  const ec = `${e} ${c}`;
+  const dc = `${d} ${c}`;
+  const t = "\nthere is still";
+  const as = "there will be an answer";
+    return `when i find myself in times of trouble${b}${d} ${c}, ${a}\nand in my hour of darkness\nshe is standing right in front of me\n${dc}, ${a}${aa}${ec}, ${a}\nand when the broken-hearted people\nliving in the world agree\n${as}, ${a}\nfor though they may be parted${t} a chance that they will see\n${as}, ${a}${aa}yeah, ${as}, ${a}${aa}\n${ec}, ${a}${ay}${ec}, ${a}\nand when the night is cloudy${t} a light that shines on me\nshine on until tomorrow, ${a}\ni wake up to the sound of music,${b}${dc}, ${a}${ay}\noh, ${as}, ${a}${ay}${ec}, ${a}`;
+  } 
+
+  
+
+  

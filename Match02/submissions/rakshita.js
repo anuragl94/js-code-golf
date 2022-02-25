3182133d1680a0bf9f9829986a6acda271bc2aa8
raks999 <rakshita.middha@zopsmart.com>
2022-02-25 12:22:02 +0530

    change made by me

--- Match02/submissions/rakshita.js
+++ Match02/submissions/rakshita.js
@@ -1,9 +1,5 @@
 function removeDuplicateCharacters(string) {
-    return string
-      .split('')
-      .filter(function(item, pos, self) {
-        return self.indexOf(item) == pos;
-      })
-      .join('');
+    return string.split('').filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;}).join('');
   }
 document.write(removeDuplicateCharacters('This is rakshita and this file is edited by rakshita'));

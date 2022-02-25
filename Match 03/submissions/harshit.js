81d9368a01ec13102b885875ee7afd1cb8b38bb7
harshitpant26 <harshit.pant@zopsmart.com>
2022-02-25 12:56:16 +0530

    added min files to  match 2 and match 3

---
+++ Match 03/submissions/harshit.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    let n1=a.length;
+      let n2=a.length;
+      if(n1!=n2)
+      return false;
+      a.split('').sort();
+      b.split('').sort();
+      for(let i=0;i<n1;i++)
+      {
+          if(a[i]!=b[i])
+              return false;
+      return true;
+      }
+  }

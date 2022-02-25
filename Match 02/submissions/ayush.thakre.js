87619bb73a47248a10700ee19ea49e4432a04ab5
ayushZopSmart <ayush.thakre@zopsmart.com>
2022-02-25 15:29:07 +0530

    linter error fixed

--- Match 02/submissions/ayush.thakre.js
+++ Match 02/submissions/ayush.thakre.js
@@ -1,13 +1,15 @@
 const a = "let it be";
-const aa = `\n${a}, ${a}\n${a}, ${a}\n`;
-const ay = `\n${a}, ${a}\n${a}, yeah, ${a}\n`;
+const n = '\n'
+const k = ', ';
+const aa = n+a+k+a+n+k+a+n;
+const ay = n+a+k+a+n+a+k+'yeah'+k+a+n;
 const b = "\nmother mary comes to me";
 const c = "words of wisdom";
 const d = "\nspeaking";
 const e = "\nwhisper";
-const ec = `${e} ${c}`;
-const dc = `${d} ${c}`;
+const ec = e+c;
+const dc = d+c
 const t = "\nthere is still";
 const as = "there will be an answer";
 const solve = () =>
-  `when i find myself in times of trouble${b}${d} ${c}, ${a}\nand in my hour of darkness\nshe is standing right in front of me\n${dc}, ${a}${aa}${ec}, ${a}\nand when the broken-hearted people\nliving in the world agree\n${as}, ${a}\nfor though they may be parted${t} a chance that they will see\n${as}, ${a}${aa}yeah, ${as}, ${a}${aa}\n${ec}, ${a}${ay}${ec}, ${a}\nand when the night is cloudy${t} a light that shines on me\nshine on until tomorrow, ${a}\ni wake up to the sound of music,${b}${dc}, ${a}${ay}\noh, ${as}, ${a}${ay}${ec}, ${a}`;
+  'when i find myself in times of trouble'+b+d+' '+c+k+a+n+'and in my hour of darkness\nshe is standing right in front of me'+n+dc+k+a+aa+ec+k+a+n+'and when the broken-hearted people\nliving in the world agree'+n+as+k+a+n+'for though they may be parted${t} a chance that they will see'+n+as+k+a+aa+'yeah'+k+as+k+a+aa+n+ex+k+a+ay+ex+k+a+n+'nand when the night is cloudy${t} a light that shines on me\nshine on until tomorrow, ${a}\ni wake up to the sound of music,'+b+dc+k+a+ay+n+'oh'+k+as+k+a+ay+ec+a;

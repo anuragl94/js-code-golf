36c320413a9079b4ff9f425b037630d99acd8c65
BhanuZopsmart <bhanu.bisen@zopsmart.com>
2022-02-25 13:07:33 +0530

    all minified and none minified code changed

---
+++ Match 02/submissions/Bhanu.Bisen.js
@@ -0,0 +1,42 @@
+function solution() {
+var l='let it be';
+var w='words of wisdom';
+var a='there will be an answer';
+var t='there is still';
+return `when i find myself in times of trouble
+mother mary comes to me
+speaking ${w}, ${l}
+and in my hour of darkness
+she is standing right in front of me
+speaking ${w}, ${l}
+${l}, ${l}
+${l}, ${l}
+whisper ${w}, ${l}
+and when the broken-hearted people
+living in the world agree
+${a}, ${l}
+for though they may be parted
+${t} a chance that they will see
+${a}, ${l}
+${l}, ${l}
+${l}, ${l}
+yeah, ${a}, ${l}
+${l}, ${l}
+${l}, ${l}
+whisper ${w}, ${l}
+${l}, ${l}
+ah, ${l}, yeah,${l}
+whisper ${w}, ${l}
+and when the night is cloudy
+${t} a light that shines on me
+shine on until tomorrow, ${l}
+i wake up to the sound of music,
+mother mary comes to me
+speaking ${l}, ${l}
+${l}, ${l}
+${l}, yeah, ${l}
+oh, ${t}, ${l}
+${l}, ${l}
+${l}, yeah, ${l}
+whisper ${w}, ${l}`;
+}

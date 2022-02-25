e191d1cd6fafbc02131b0b636df13e0b6cf3b43c
Ayush Agrawal <98871208+ayush070zopsmart@users.noreply.github.com>
2022-02-25 18:22:39 +0530

    Deleted unnecessary files

--- Match 03/submissions/ayush.agrawal@zopsmart.com-ayush070zopsmart.js
+++
@@ -1,9 +0,0 @@
-function solution(a, b) {
-    if (a.length == b.length && !(/[^a-zA-Z]/.test(a) || (/[^a-zA-Z]/.test(b)))) {
-        a = a.split("").sort();
-        b = b.split("").sort();
-        return JSON.stringify(a) === JSON.stringify(b);
-    } else {
-        return false;
-    }
-}

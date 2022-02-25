635d54b81af5b4a450b25e5056fa7655646ed93a
raks999 <rakshita.middha@zopsmart.com>
2022-02-25 12:38:22 +0530

    minified solution for 1,2,3

--- Match02/submissions/rakshita.js
+++
@@ -1,5 +0,0 @@
-function removeDuplicateCharacters(string) {
-    return string.split('').filter(function(item, pos, self) {
-        return self.indexOf(item) == pos;}).join('');
-  }
-document.write(removeDuplicateCharacters('This is rakshita and this file is edited by rakshita'));

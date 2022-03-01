ebede28acf876684efd45ad92be293b4df4ba698
Divij-Agarwal <99388306+Divij-Agarwal@users.noreply.github.com>
2022-03-01 13:00:48 +0530

    Divij's submission for Match2 & Match3 (#25)

---
+++ Match 02/submissions/Divij.Agarwal.js
@@ -0,0 +1,12 @@
+const a = "let it be";
+const a1 = `\n${a}, ${a}\n${a}, ${a}\n`;
+const b = "\nmother mary comes to me";
+const c = "words of wisdom";
+const d = "\nspeaking";
+const e = "\nwhisper";
+const dc = `${d} ${c}`;
+const t = "\nthere is still";
+const as = "there will be an answer";
+function solution() {
+    return `when i find myself in times of trouble${b}${d} ${c}, ${a}\nand in my hour of darkness\nshe is standing right in front of me\n${dc}, ${a}${a1}${e} ${c}, ${a}\nand when the broken-hearted people\nliving in the world agree\n${as}, ${a}\nfor though they may be parted${t} a chance that they will see\n${as}, ${a}${a1}yeah, ${as}, ${a}${a1}\n${e} ${c}, ${a}\n${a}, ${a}\n${a}, yeah, ${a}\n${e} ${c}, ${a}\nand when the night is cloudy${t} a light that shines on me\nshine on until tomorrow, ${a}\ni wake up to the sound of music,${b}${dc}, ${a}\n${a}, ${a}\n${a}, yeah, ${a}\n\noh, ${as}, ${a}\n${a}, ${a}\n${a}, yeah, ${a}\n${e} ${c}, ${a}`;
+  }

18c8d1c086ca4b9e9a0b007fd4cad1182aeb6918
ayushZopsmart <98867416+ayushZopsmart@users.noreply.github.com>
2022-03-01 13:02:04 +0530

    Ayush's submission for  Match2 & Match3 (#18)
    * assignment solved match2 & match2
    
    * assignment solved match2 & match3
    
    * changed match2 solution
    
    * code cleanup
    
    * further minfied the files
    
    * linter error fixed

---
+++ Match 02/submissions/ayush.thakre.js
@@ -0,0 +1,15 @@
+const a = "let it be";
+const n = '\n'
+const k = ', ';
+const aa = n+a+k+a+n+k+a+n;
+const ay = n+a+k+a+n+a+k+'yeah'+k+a+n;
+const b = "\nmother mary comes to me";
+const c = "words of wisdom";
+const d = "\nspeaking";
+const e = "\nwhisper";
+const ec = e+c;
+const dc = d+c
+const t = "\nthere is still";
+const as = "there will be an answer";
+const solve = () =>
+  'when i find myself in times of trouble'+b+d+' '+c+k+a+n+'and in my hour of darkness\nshe is standing right in front of me'+n+dc+k+a+aa+ec+k+a+n+'and when the broken-hearted people\nliving in the world agree'+n+as+k+a+n+'for though they may be parted${t} a chance that they will see'+n+as+k+a+aa+'yeah'+k+as+k+a+aa+n+ex+k+a+ay+ex+k+a+n+'nand when the night is cloudy${t} a light that shines on me\nshine on until tomorrow, ${a}\ni wake up to the sound of music,'+b+dc+k+a+ay+n+'oh'+k+as+k+a+ay+ec+a;
